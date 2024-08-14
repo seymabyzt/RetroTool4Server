6fc19b6a6f3347b5bba90730faa6c8062729e067
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-08-14 18:38:15 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 
-import { Server } from "socket.io"
-
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const { Server } = require("socket.io");
+
 const app = express()
 app.use(cors())
 
