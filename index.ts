51300070d8df4d44cb585ba58d08fd27000a0a86
Seyma Bayezit <163021404+seymabyzt@users.noreply.github.com>
2024-08-14 19:09:20 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,7 +11,8 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://retro-tool4.vercel.app",
+        origin: [
+            "https://retro-tool4.vercel.app","http://localhost:3000"],
         methods: ["GET", "POST"],
     },
 })
